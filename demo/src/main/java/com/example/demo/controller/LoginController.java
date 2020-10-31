8aa8c356b024ee943cd281922ba00b893747db73
Ryanwin <976994713@qq.com>
2020-10-31 16:32:09 +0800

    环境配置基本完成

--- demo/src/main/java/com/example/demo/controller/LoginController.java
+++ demo/src/main/java/com/example/demo/controller/LoginController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.pojo.User;
 import com.example.demo.result.Result;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.HtmlUtils;
@@ -9,6 +11,10 @@ import java.util.Objects;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    UserService userService;
+
     @CrossOrigin
     @PostMapping(value = "api/login")
     @ResponseBody
@@ -17,9 +23,8 @@ public class LoginController {
         String username = requestUser.getUsername();
         username = HtmlUtils.htmlEscape(username);
 
-        if (!Objects.equals("admin", username) || !Objects.equals("123456", requestUser.getPassword())) {
-            String message = "账号密码错误";
-            System.out.println("test");
+        User user = userService.get(username, requestUser.getPassword());
+        if (null == user) {
             return new Result(400);
         } else {
             return new Result(200);
