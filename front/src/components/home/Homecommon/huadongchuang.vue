7ca26f4204f7cbdf4aea43c63e55f16cf4d2c5ec
爱情陷阱 <2211463127@qq.com>
2020-11-16 16:24:52 +0800

    enter

---
+++ front/src/components/home/Homecommon/huadongchuang.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-carousel :interval="4000" type="card" height="400px">
+    <el-carousel-item  class="carousel-item" v-for="item in imagesbox" :key="item.id">
+      <img :src="item.idView" class="image">
+     
+    </el-carousel-item>
+  </el-carousel>
+</template>
+
+<style>
+   .carousel-item {
+    width: 100%;
+    height: 100%;
+    background: #99a9bf;
+    display: flex;
+    justify-content: center;
+  }
+
+  .image {
+    max-width: 100%;
+    max-height: 100%;
+  }
+</style>
+
+<script> 
+  export default{
+    data(){
+      return {
+        imagesbox:[{id:0,idView:require("D:/ITT/itproject/front/src/assets/P1.jpg")}, 
+                   {id:1,idView:require("D:/ITT/itproject/front/src/assets/P2.jpg")},
+                  {id:2,idView:require("D:/ITT/itproject/front/src/assets/P3.jpg")},
+                  {id:3,idView:require("D:/ITT/itproject/front/src/assets/P4.jpg")},
+                   ]
+      }
+    },
+    methods: {
+      
+    },
+    mounted() {
+    }
+  }
+</script>
+<style>
+</style>
---
+++ front/src/components/home/Homecommon/sousuokuang.vue
@@ -0,0 +1,34 @@
+<template>
+   <div style="margin-top: 15px;">
+  <el-input placeholder="查找图书" v-model="input5" class="input-with-select">
+    <el-select v-model="select" slot="prepend" placeholder="请选择">
+      <el-option label="书名" value="1"></el-option>
+      <el-option label="出版社" value="2"></el-option>
+      <el-option label="作者" value="3"></el-option>
+    </el-select>
+    <el-button slot="append" icon="el-icon-search"></el-button>
+  </el-input>
+</div>
+</template>
+
+
+<style>
+  .el-select .el-input {
+    width: 130px;
+  }
+  .input-with-select .el-input-group__prepend {
+    background-color:#fff;
+  }
+
+ 
+</style>
+<script>
+export default {
+  data() {
+    return {
+      input5: '',
+      select: ''
+    }
+  }
+}
+</script>
